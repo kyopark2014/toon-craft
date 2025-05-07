aadaa41c8f22d1aa8aa70fd1b0977e375f52d1de
John Park <kyopark2014@gmail.com>
2025-05-07 20:00:19 +0900

    update

---
+++ cdk-toon-craft/lib/cdk-toon-craft-stack.ts
@@ -0,0 +1,58 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as path from "path";
+
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+const region = process.env.CDK_DEFAULT_REGION;    
+const accountId = process.env.CDK_DEFAULT_ACCOUNT;
+const projectName = `toon-craft`; 
+
+export class CdkToonCraftStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // lambda-tool
+    const roleLambdaTools = new iam.Role(this, `role-lambda-tools-for-${projectName}`, {
+      roleName: `role-lambda-tools-for-${projectName}-${region}`,
+      assumedBy: new iam.CompositePrincipal(
+        new iam.ServicePrincipal("lambda.amazonaws.com"),
+        new iam.ServicePrincipal("bedrock.amazonaws.com"),
+      ),
+      // managedPolicies: [cdk.aws_iam.ManagedPolicy.fromAwsManagedPolicyName('CloudWatchLogsFullAccess')] 
+    });
+    // roleLambdaTools.addManagedPolicy({  // grant log permission
+    //   managedPolicyArn: 'arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole',
+    // });
+    const CreateLogPolicy = new iam.PolicyStatement({  
+      resources: [`arn:aws:logs:${region}:${accountId}:*`],
+      actions: ['logs:CreateLogGroup'],
+    });        
+    roleLambdaTools.attachInlinePolicy( 
+      new iam.Policy(this, `create-log-policy-lambda-tools-for-${projectName}`, {
+        statements: [CreateLogPolicy],
+      }),
+    );
+    const CreateLogStreamPolicy = new iam.PolicyStatement({  
+      resources: [`arn:aws:logs:${region}:${accountId}:log-group:/aws/lambda/*`],
+      actions: ["logs:CreateLogStream","logs:PutLogEvents"],
+    });        
+    roleLambdaTools.attachInlinePolicy( 
+      new iam.Policy(this, `create-stream-log-policy-lambda-tools-for-${projectName}`, {
+        statements: [CreateLogStreamPolicy],
+      }),
+    ); 
+
+    const lambdaName = `toons-craft-upload`;
+    const lambdaTools = new lambda.DockerImageFunction(this, `lambda-tools-for-${projectName}`, {
+      description: 'toons-craft upload api',
+      functionName: lambdaName,
+      code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../../lambda-upload')),
+      timeout: cdk.Duration.seconds(180),
+      role: roleLambdaTools,
+      environment: {
+      }
+    });
+  }
+}
