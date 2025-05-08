7f86b1d06ba86195826429be24be78666f77de63
John Park <kyopark2014@gmail.com>
2025-05-08 20:05:18 +0900

    update

--- lambda-retrieve/lambda_function.py
+++ lambda-retrieve/lambda_function.py
@@ -558,7 +558,7 @@ def lambda_handler(event, context):
     for video in video_list:
         #print(f"image: {image}")
         video = video.replace(video_prefix, '')
-        print(f"video: {video}")
+        # print(f"video: {video}")
             
         item_data = parse_object_name(video, video_prefix)    
         if item_data:    
