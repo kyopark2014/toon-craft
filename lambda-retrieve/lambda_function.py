57d3373b15ea2ef8044e84deeef495a01791244f
Jisun Choi <jschoii@amazon.com>
2025-05-09 11:28:18 +0900

    Revert "recommend prompt revision"
    This reverts commit 88a0eab56245379b2fda656459d0de947d552049.

--- lambda-retrieve/lambda_function.py
+++ lambda-retrieve/lambda_function.py
@@ -540,8 +540,7 @@ def lambda_handler(event, context):
     # How about a Korean dish with harmonious flavors, just like savoring different types of coffee? The vibrant colors will satisfy your artistic sensibility, and the diverse flavor combinations will stimulate your scientific curiosity. Feel the thrill of success in your mouth, and make today's joy even more special. May your evening shine as brightly as your brilliant idea!
     # """
 
-    # search_keyword = extract_txt_block(recommend)
-    search_keyword = recommend["food_query"]
+    search_keyword = extract_txt_block(recommend)
 
     # Get embedding for user input
     user_embedding = embedding_client.embedding_text(search_keyword)
